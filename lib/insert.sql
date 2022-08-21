02c2ca1e6088704bccbfae95ff7c6f5ebb3b7fd4
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-08-21 22:18:35 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, sex) VALUES
+(1,"Mr. Chocolate", "M"),
+(2,"Rowdy", "M"),
+(3,"Tabitha", "F"),
+(4,"Sargeant Brown", "M"),
+(5,"Melissa", "F"),
+(6,"Grinch","M"),
+(7,"Wendy", "F"),
+(8,null, "M");
